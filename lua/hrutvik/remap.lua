78935775fc356dc07606d67816cd99a721e69ea0
Hrutvik Yadav <hrutvikyadav@gmail.com>
2023-07-14 23:22:06 +0530

    feat: added mappings for diagnostics, also for LSP functions and some formatting config if needed for later

--- lua/hrutvik/remap.lua
+++ lua/hrutvik/remap.lua
@@ -31,3 +31,8 @@ vim.keymap.set("n", "<Right>", ">>")
 vim.keymap.set("v", "<Right>", ">gv")
 vim.keymap.set("v", "<Left>", "<gv")
 
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
+
