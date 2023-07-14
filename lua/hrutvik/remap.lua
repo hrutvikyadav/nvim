4c998d1605ebf38942c6da3769f60e6f86e0618d
Hrutvik Yadav <hrutvikyadav@gmail.com>
2023-07-15 01:09:31 +0530

    feat: added descriptions to keymaps for which-key, added more Telescope keymaps

--- lua/hrutvik/remap.lua
+++ lua/hrutvik/remap.lua
@@ -1,9 +1,9 @@
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>dl", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>dl", vim.cmd.Ex, { desc = 'NetRW: [d]irectory [l]isting' })
 
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
-
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = 'move visual selection down' })
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = 'move visual selection up' })
+-- keep cursor in place when navigating with below motions
 vim.keymap.set("n", "J", "mzJ`z")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
@@ -24,7 +24,8 @@ vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
 vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
-vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]],
+    { desc = 'EX: multicursor find and replace' })
 
 vim.keymap.set("n", "<Left>", "<<")
 vim.keymap.set("n", "<Right>", ">>")
@@ -35,4 +36,3 @@ vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous dia
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
-
