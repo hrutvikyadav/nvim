c4b47878591a6db9e821e1df6bdd3bdaccd5c0c5
Hrutvik Yadav <hrutvikyadav@gmail.com>
2023-07-15 01:09:31 +0530

    feat: added descriptions to keymaps for which-key, added more Telescope keymaps

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -1,10 +1,10 @@
 local mark = require("harpoon.mark")
 local ui = require("harpoon.ui")
 
-vim.keymap.set("n", "<leader>a", mark.add_file) 
-vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
+vim.keymap.set("n", "<leader>a", mark.add_file, { desc = '[a]dd to harpoon list' })
+vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu, { desc = '[e]xplore harpoon list' })
 
-vim.keymap.set("n", "<leader>1", function() ui.nav_file(1) end)
-vim.keymap.set("n", "<leader>2", function() ui.nav_file(2) end)
-vim.keymap.set("n", "<leader>3", function() ui.nav_file(3) end)
-vim.keymap.set("n", "<leader>4", function() ui.nav_file(4) end)
+vim.keymap.set("n", "<leader>1", function() ui.nav_file(1) end, { desc = 'navigate [1]st item in harpoon list' })
+vim.keymap.set("n", "<leader>2", function() ui.nav_file(2) end, { desc = 'navigate [2]nd item in harpoon list' })
+vim.keymap.set("n", "<leader>3", function() ui.nav_file(3) end, { desc = 'navigate [3]rd item in harpoon list' })
+vim.keymap.set("n", "<leader>4", function() ui.nav_file(4) end, { desc = 'navigate [4]th item in harpoon list' })
--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,4 +1,7 @@
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>sf', builtin.find_files, {})
-vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-vim.keymap.set('n', '<leader>sg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = 'TELESCOPE [s]earch pwd [f]iles' })
+vim.keymap.set('n', '<C-p>', builtin.git_files, { desc = 'TELESCOPE git files' })
+vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = 'TELESCOPE live grep pwd files' })
+
+vim.keymap.set('n', '<leader>Gc', builtin.git_commits, { desc = 'TELESCOPE [G]it [c]ommits' })
+vim.keymap.set('n', '<leader>Gb', builtin.git_branches, { desc = 'TELESCOPE [G]it [b]ranches' })
