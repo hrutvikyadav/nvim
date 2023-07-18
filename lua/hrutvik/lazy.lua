34648a410ecd1c21c71744ddcd4af076a04e10f4
hrutvikyadav <97113293+hrutvikyadav@users.noreply.github.com>
2023-07-18 12:09:24 +0530

    feat: added autopairs plugin (#4)
    * feat: added mappings for diagnostics, also for LSP functions and some
    formatting config if needed for later
    
    * feat: added descriptions to keymaps for which-key, added more Telescope
    keymaps
    
    * feat: added autopairs plugin for completing closing braces/strings etc
    automatically

--- lua/hrutvik/lazy.lua
+++ lua/hrutvik/lazy.lua
@@ -113,6 +113,13 @@ require("lazy").setup({
             },
         },
     },
+    {
+        'windwp/nvim-autopairs',
+        event = "InsertEnter",
+        opts = {
+            disable_filetype = { "TelescopePrompt" , "vim" },
+        } -- this is equalent to setup({}) function
+    },
 })
 
 -- vim.cmd('colorscheme rose-pine')
