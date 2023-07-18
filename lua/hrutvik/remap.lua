9c355f3c2633e81bc92085350962ed9dcd173020
hrutvikyadav <hrutvikyadav@gmail.com>
2023-07-18 14:22:23 +0530

    feat: added remap for git push

--- lua/hrutvik/remap.lua
+++ lua/hrutvik/remap.lua
@@ -36,3 +36,5 @@ vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous dia
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
+
+vim.keymap.set('n', "<leader>Gp", "<cmd>G push<cr>", { desc = '[G]it [p]ush to upstream' })
